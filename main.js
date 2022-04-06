0aab59e7332465025fc0bb1720afd82905409f89
Chris Yamada <christopher@yamada-san.org>
2022-04-06 16:44:12 -0700

    First Commit from command line, initialized arrays for random message

---
+++ main.js
@@ -0,0 +1,6 @@
+// project goal, use git/github from command line to version control this project
+// array to hold the random components of the message
+const noun = ['Cat','Dog','Fish','Bird','Lion','Elephant','Spider','Worm','Ant'];
+const verb = ['gets','eats','finds','loves','beats','teaches'];
+
+
