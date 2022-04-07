182bfd9eb237702ed4458bafb9fe77b7220d2e62
Chris Yamada <christopher@yamada-san.org>
2022-04-06 18:00:52 -0700

    Second commit, added functionality for random phrase

--- main.js
+++ main.js
@@ -3,4 +3,9 @@
 const noun = ['Cat','Dog','Fish','Bird','Lion','Elephant','Spider','Worm','Ant'];
 const verb = ['gets','eats','finds','loves','beats','teaches'];
 
+let nounOne = noun[Math.floor(Math.random()*noun.length)];
+let verbOne = verb[Math.floor(Math.random()*verb.length)];
+let nounTwo = noun[Math.floor(Math.random()*noun.length)];
+
+console.log(`The ${nounOne} ${verbOne} the ${nounTwo}`);
 
